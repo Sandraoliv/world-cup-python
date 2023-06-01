ca8bd24b11ad3463eb407949ee88198519a25c32
sandraoliv <sandra.so219@gmail.com>
2023-06-01 13:35:59 -0300

    feat: tarefa 2 finalizada

--- utils.py
+++ utils.py
@@ -7,12 +7,12 @@ def data_processing(data):
 
     if titles < 0:
         raise NegativeTitlesError()
-    
+
     first_cup_year = int(first_cup[:4])
     if first_cup_year < 1930:
         raise InvalidYearCupError()
-    
-    current_year = 2023  
+
+    current_year = 2023
     max_possible_titles = (current_year - first_cup_year) // 4 + 1
     if titles > max_possible_titles:
         raise ImpossibleTitlesError()
